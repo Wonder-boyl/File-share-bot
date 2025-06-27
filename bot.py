ed407eeb43fe479911f5f44111f12b4e84b0e26a
Wonder-boyl <hellyed234@gmail.com>
2025-06-27 12:29:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,13 +21,13 @@ from config import (
 
 class Bot(Client):
     def __init__(self):
-        super().__init__('self')
-            name={"Bot"},
-            api_hash={"6ddf5d5aa190b0f818e3731a39bd407d"},
-            api_id={"8465056"},
+        super().__init__(
+            name="Bot",
+            api_hash="6ddf5d5aa190b0f818e3731a39bd407d",
+            api_id="8465056",
             plugins={"root": "plugins"},
-            workers={"5519278195"},
-            bot_token={"7934086431:AAExbaMy0BfTlzE0WQI805HsVpY9sjakZD0"},
+            workers="5519278195",
+            bot_token="7934086431:AAExbaMy0BfTlzE0WQI805HsVpY9sjakZD0",
         )
         self.LOGGER = LOGGER
 
