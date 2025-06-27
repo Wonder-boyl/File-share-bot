124090ef9a82a3b1bea67b2165059aa0c982538f
Wonder-boyl <hellyed234@gmail.com>
2025-06-27 12:10:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
 from bot import Bot
-Bot.__init__('self')
-super().__init__('self')
-name="Bot",
-api_hash="6ddf5d5aa190b0f818e3731a39bd407d",
-api_id=8465056,
-plugins={"root": "plugins"},
-workers=5519278195,
-bot_token="7934086431:AAExbaMy0BfTlzE0WQI805HsVpY9sjakZD0"
+Bot.__init__(self)
