38464075bc0d3483c071dc4bdd937d2b9a943bf0
Wonder-boyl <hellyed234@gmail.com>
2025-06-27 11:59:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ from config import (
 class Bot(Client):
     def __init__(self):
         super().__init__('self')
-            name="self",
+            name="file-sharing-man",
             api_hash="6ddf5d5aa190b0f818e3731a39bd407d",
             api_id=8465056,
             plugins={"root": "plugins"},
