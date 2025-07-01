3621e3ee9feff227091fcbfc787937fad1c07096
Wonder-boyl <hellyed234@gmail.com>
2025-07-01 20:17:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,6 @@ api_id="8465056",
 plugins={"root": "plugins"},
 workers="5519278195",
 bot_token="7934086431:AAExbaMy0BfTlzE0WQI805HsVpY9sjakZD0",
-        
-self.LOGGER = LOGGER
 
 async def start(self):
         try:
